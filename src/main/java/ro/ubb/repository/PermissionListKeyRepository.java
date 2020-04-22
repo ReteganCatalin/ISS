9f423e16f19b269115e602e73c367a8ac8426cc4
Ravasz Tamas <ravaszt88@gmail.com>
2020-04-22 15:30:36 +0300

    Keys don't need the repo

--- src/main/java/ro/ubb/repository/PermissionListKeyRepository.java
+++
@@ -1,7 +0,0 @@
-package ro.ubb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ro.ubb.domain.PermissionListKey;
-
-public interface PermissionListKeyRepository  extends JpaRepository<PermissionListKey,Integer> {
-}
--- src/main/java/ro/ubb/repository/ProposalListKeyRepository.java
+++
@@ -1,7 +0,0 @@
-package ro.ubb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ro.ubb.domain.ProposalListKey;
-
-public interface ProposalListKeyRepository  extends JpaRepository<ProposalListKey,Integer> {
-}
