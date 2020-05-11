573aaffea3a2119c4d721220d707e305750b4f5a
Sarmaluta <raulrusu2014@gmail.com>
2020-05-11 18:08:53 +0300

    conference page info only gui

--- client/CMS/src/app/main-page/main-page-routing.module.ts
+++ client/CMS/src/app/main-page/main-page-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {MainPageComponent} from './main-page.component';
 import {ConferencesComponent} from './pages/conferences/conferences.component';
+import {ConferenceComponent} from './pages/conference/conference.component';
 
 
 const mainPageRoutes: Routes = [
   {
     path: '', component: MainPageComponent, children: [
       {path: 'conferences', component: ConferencesComponent},
+      {path: 'conferences/conference', component: ConferenceComponent},
       {path: '', redirectTo: 'conferences', pathMatch: 'full'}
 
       ]
--- client/CMS/src/app/main-page/main-page.module.ts
+++ client/CMS/src/app/main-page/main-page.module.ts
@@ -4,16 +4,21 @@ import { ConferencesComponent } from './pages/conferences/conferences.component'
 import { MainPageRoutingModule } from './main-page-routing.module';
 import {AddConferenceComponent} from './custom-components/add-conference/add-conference.component';
 import {FormsModule} from '@angular/forms';
+import { ConferenceComponent } from './pages/conference/conference.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
     ConferencesComponent,
-    AddConferenceComponent
+    AddConferenceComponent,
+    ConferenceComponent
   ],
   imports: [
     CommonModule,
     MainPageRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ]
 })
 export class MainPageModule { }
