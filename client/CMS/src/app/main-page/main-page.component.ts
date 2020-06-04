37b06287e727ca74fde93e659857aca3e19f6b8d
Sarmaluta <raulrusu2014@gmail.com>
2020-06-04 17:06:14 +0300

    feeling cute might delete this later

--- client/CMS/src/app/main-page/main-page.component.ts
+++ client/CMS/src/app/main-page/main-page.component.ts
@@ -11,6 +11,7 @@ import {
 import {AddConferenceComponent} from './custom-components/add-conference/add-conference.component';
 import {ConferenceService} from "../shared/services/conference.service";
 import {UserService} from "../shared/services/user.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-main-page',
@@ -25,7 +26,9 @@ export class MainPageComponent implements OnInit, AfterViewInit{
   constructor(private resolver: ComponentFactoryResolver,
               private changeDetectorRef: ChangeDetectorRef,
               private conferenceService: ConferenceService,
-              private applicationRef: ApplicationRef, private userService: UserService) {
+              private applicationRef: ApplicationRef,
+              private userService: UserService,
+              private http: HttpClient) {
     this.conferenceList = [];
     this.visible = 1;
   }
@@ -33,15 +36,15 @@ export class MainPageComponent implements OnInit, AfterViewInit{
   ngOnInit(): void {
     let uid = +sessionStorage.getItem('uid');
     this.userService.setUserID(uid);
-    this.conferenceService.getConferenceNamesWithIDs().subscribe(response => {
+    this.http.get<any>(`http://localhost:8081/pc_members/conferences/${uid}`).subscribe(response => {
       this.conferenceList = response;
-    })
+    });
   }
 
   ngAfterViewInit() {
     const formFormFactory = this.resolver.resolveComponentFactory(AddConferenceComponent);
     this.entry.createComponent(formFormFactory);
-    this.changeDetectorRef.detectChanges(); // nu stiu ce face line asta, da trebe ca sa nu isi dea naspa :/
+    this.changeDetectorRef.detectChanges();
   }
 
   selectVisible(i) {
--- client/CMS/src/app/main-page/pages/my-conference/my-conference.component.ts
+++ client/CMS/src/app/main-page/pages/my-conference/my-conference.component.ts
@@ -1,20 +1,35 @@
 import {AfterContentInit, ChangeDetectorRef, Component, ContentChildren, Input, OnInit} from '@angular/core';
 import {BiddingPageComponent} from "./tabs/bidding-page/bidding-page.component";
 import { ActivatedRoute } from '@angular/router';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-my-conference',
   templateUrl: './my-conference.component.html',
   styleUrls: ['./my-conference.component.css']
 })
-export class MyConferenceComponent implements AfterContentInit{
+export class MyConferenceComponent implements AfterContentInit, OnInit{
   @ContentChildren(BiddingPageComponent) biddingPage: BiddingPageComponent;
   @Input("conferenceID") conferenceID;
+  conference;
+  today;
   constructor(private routes: ActivatedRoute,
-              private  changeDetectorRef: ChangeDetectorRef) {
-
+              private  changeDetectorRef: ChangeDetectorRef,
+              private http:HttpClient) {
+    this.today = new Date();
+    console.log(this.today);
   }
 
+  ngOnInit(): void {
+    this.http.get(`http://localhost:8081//conferences/${this.conferenceID}`).subscribe(result => {
+      this.conference = result;
+      console.log(this.conference);
+    })
+  }
+
+
+
   ngAfterContentInit() {
+
   }
 }
